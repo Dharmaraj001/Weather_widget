372ebf03fa84d0bc1ea97a2223d11c62680a8b2c
Dharmaraj001 <dharmarajs877@gmail.com>
2026-02-23 15:37:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Searchbox from './Searchbox'
+
+const App = () => {
+  return (
+    <>
+      <Searchbox/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Searchbox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export default function Searchbox() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300">
+      
+      <div className="bg-white shadow-xl rounded-2xl p-8 w-[350px] text-center">
+        
+        <h3 className="text-2xl font-semibold text-gray-700 mb-6">
+          Search Weather
+        </h3>
+
+        <form className="flex flex-col gap-4">
+          
+          <TextField
+            id="City"
+            label="City"
+            variant="outlined"
+            required
+            fullWidth
+          />
+
+          <Button
+            variant="contained"
+            type="submit"
+            className="!bg-blue-500 hover:!bg-blue-600 !text-white !font-semibold !py-2 !rounded-lg"
+          >
+            Search
+          </Button>
+
+        </form>
+
+      </div>
+
+    </div>
+  );
+}
